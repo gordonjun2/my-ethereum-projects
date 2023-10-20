73a45c8d8d0873e996c2e387cbb7809718a3e4a5
gordonjun <gordonoh@yahoo.com.sg>
2023-10-20 23:59:32 +0800

    continue with course

--- Udemy-Ethereum-and-Solidity-The-Complete-Developer-Guide/Section-8-Appendix-Basic-React/ReduxSimpleStarter/src/components/video_list_item.js
+++ Udemy-Ethereum-and-Solidity-The-Complete-Developer-Guide/Section-8-Appendix-Basic-React/ReduxSimpleStarter/src/components/video_list_item.js
@@ -1,9 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>React simple starter</div>
-    );
-  }
-}
+const VideoListItem = ({video}) => {
+  const imageUrl = video.snippet.thumbnails.default.url;
+
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={imageUrl} />
+        </div>
+        <div className="media-body">
+          <div className="media-heading">{video.snippet.title}</div>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default VideoListItem;
--- Udemy-Ethereum-and-Solidity-The-Complete-Developer-Guide/Section-8-Appendix-Basic-React/ReduxSimpleStarter/src/index.js
+++ Udemy-Ethereum-and-Solidity-The-Complete-Developer-Guide/Section-8-Appendix-Basic-React/ReduxSimpleStarter/src/index.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
-import SearchBar from './components/search_bar'
+// const API_KEY = "YOUR_YOUTUBE_DATA_API_KEY";
+const API_KEY = "AIzaSyDUirOkOloEwIaFgeLfHpVeJjPEvsfU0vM";
 
-const API_KEY = "YOUR_YOUTUBE_DATA_API_KEY";
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'three kingdoms'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={ this.state.videos } />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
