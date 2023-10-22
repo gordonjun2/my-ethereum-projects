bc26631a7ad03419f8059d4d7d2829b23bd810fe
gordonjun <gordonoh@yahoo.com.sg>
2023-10-22 22:49:23 +0800

    continue with course

--- Udemy-Ethereum-and-Solidity-The-Complete-Developer-Guide/Section-6-Ethereum-Project-Infrastructure/kickstart/ethereum/contracts/Campaign.sol
+++ Udemy-Ethereum-and-Solidity-The-Complete-Developer-Guide/Section-6-Ethereum-Project-Infrastructure/kickstart/ethereum/contracts/Campaign.sol
@@ -40,7 +40,7 @@ contract Campaign {
     }
 
     function contribute() public payable {
-        require(msg.value > minimumContribution);
+        require(msg.value >= minimumContribution);
 
         approvers[msg.sender] = true;
         approversCount++;
